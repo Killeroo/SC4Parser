47235b0007429225071b410c85b5195f34ac367e
Matthew Carney <matthewcarney64@gmail.com>
2020-07-21 23:27:42 +0100

    More work on building subfile

--- TestParser/Structures/SubFiles/Building.cs
+++ TestParser/Structures/SubFiles/Building.cs
@@ -9,9 +9,16 @@ namespace TestParser.Structures.SubFiles
     //https://wiki.sc4devotion.com/index.php?title=Building_Subfile
     class Building
     {
+        public uint Size;
+        public uint CRC;
+        public uint Memory;
+        public ushort MajorVersion;
+        public ushort MinorVersion;
+        public ushort ZotWord;
+        public byte AppearanceFlag;
+        
         public void Parse(byte[] buffer)
         {
-
         }
     }
 }
---
+++ TestParser/Structures/SubFiles/BuildingsFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParser.Structures.SubFiles
+{
+    class BuildingsFile
+    {
+        List<Building> Buildings = new List<Building>();
+
+        public void Parse(byte[] buffer)
+        {
+
+        }
+
+        public void Dump()
+        {
+
+        }
+    }
+}
