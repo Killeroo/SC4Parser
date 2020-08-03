5abe5d19a8a1d94934f731a758658e3b9b19b6fd
Matthew Carney <matthewcarney64@gmail.com>
2020-08-03 23:19:39 +0100

    Some cleaning up stuff

--- SC4Cartographer/Program.cs
+++ SC4Cartographer/Program.cs
@@ -42,11 +42,11 @@ namespace SC4Cartographer
 
             //128 for mid
             // 256 for large
-            CreateGridImage(256, 256, 9, 9, 60, l);
+            CreateGridImage(256, 256, 60, l);
 
     }
 
-        public static void CreateGridImage(int maxXCells, int maxYCells, int cellXPosition, int cellYPosition, int boxSize, LotSubFile lots)
+        public static void CreateGridImage(int maxXCells, int maxYCells, int boxSize, LotSubFile lots)
         {
             using (Bitmap bmp = new Bitmap(maxXCells * boxSize + 1, maxYCells * boxSize + 1))
             {
@@ -65,72 +65,7 @@ namespace SC4Cartographer
                     int count = 0;
                     foreach (var lot in lots.Lots)
                     {
-                        //Rectangle rect = new Rectangle((boxSize * lot.MaxTileX) + 5, (boxSize * lot.MaxTileZ) + 5, boxSize - 10, boxSize - 10);
-                        //Rectangle rect2 = new Rectangle((boxSize * lot.MinTileX) + 5, (boxSize * lot.MinTileZ) + 5, boxSize - 10, boxSize - 10);
-
-
-                        //Rectangle rect = new Rectangle((boxSize * lot.MaxTileX) + 5, (boxSize * lot.MaxTileZ) + 5, boxSize - 10, boxSize - 10);
-
                         Rectangle rect = new Rectangle();
-
-                        //Rectangle r = Rectangle.FromLTRB(boxSize * lot.MinTileX, (boxSize * lot.MinTileX) + (boxSize * lot.SizeX),
-                        //                                 boxSize * lot.MaxTileX, (boxSize * lot.MaxTileZ)
-                        //int startX = 0;
-                        //int startZ = 0;
-                        //int sizeX = 1;
-                        //int sizeZ = 1;
-
-                        //if (lot.MinTileX == lot.MaxTileX && lot.MinTileZ == lot.MaxTileZ)
-                        //{
-                        //    rect = new Rectangle((boxSize * lot.MaxTileX) + 5, (boxSize * lot.MaxTileZ) + 5, boxSize - 10, boxSize - 10);
-                        //}
-
-                        //if (lot.MinTileX < lot.MaxTileX
-                        //    && lot.MinTileZ > lot.MaxTileZ)
-                        //{
-                        //    sizeX = boxSize * lot.SizeX;
-                        //    startX = lot.MinTileX;
-                        //}
-
-                        //byte otherRectCorner1X = 0;
-                        //byte otherRectCorner1Z = 0;
-                        
-                        //byte otherRectCorner2X = 0;
-                        //byte otherRectCorner2Z = 0;
-
-                        //if (lot.MinTileZ < lot.MaxTileZ)
-                        //{
-                        //    otherRectCorner1Z = lot.MinTileZ;
-                        //    otherRectCorner1X = lot.MaxTileX;
-                        //}
-                        //else
-                        //{
-                        //    otherRectCorner1Z = lot.MaxTileZ;
-                        //    otherRectCorner1X = lot.MinTileX;
-                        //}
-
-                        //if (lot.MinTileX < lot.MaxTileX)
-                        //{
-                        //    otherRectCorner2X = lot.MinTileX;
-                        //    otherRectCorner2Z = lot.MaxTileZ;
-                        //}
-                        //else
-                        //{
-                        //    otherRectCorner2X = lot.MaxTileX;
-                        //    otherRectCorner2Z = lot.MinTileZ;
-                        //}
-
-
-
-                        //Console.WriteLine(lot.Offset.ToString("X"));
-                        //Console.WriteLine("{0}x{1}", lot.MinTileX, lot.MaxTileX);
-                        //Console.WriteLine("{0}x{1}", lot.MaxTileX, lot.MaxTileZ);
-                        //Console.WriteLine("dep:{0} wid:{1}", lot.SizeZ, lot.SizeX);
-                        //Console.WriteLine("-----------------");
-                        //Console.ReadLine();
-
-
-
 
                         Color c = new Color();
                         switch (lot.ZoneType)
@@ -192,51 +127,9 @@ namespace SC4Cartographer
 
                         }
 
-                        //Color c2 = new Color();
-                        //switch (lot.Orientation)
-                        //{
-                        //    case Constants.ORIENTATION_NORTH:
-                        //        c2 = Color.FromArgb(255, 100, 0);
-                        //        break;
-
-                        //    case Constants.ORIENTATION_EAST:
-
-                        //        c2 = Color.FromArgb(0, 255, 100);
-                        //        break;
-
-                        //    case Constants.ORIENTATION_SOUTH:
-
-                        //        c2 = Color.FromArgb(100, 0, 255);
-                        //        break;
-
-                        //    case Constants.ORIENTATION_WEST:
-
-                        //        c2 = Color.FromArgb(0, 0, 0);
-                        //        break;
-                        //}
-
-
                         g.FillRectangle(new SolidBrush(c), rect);
-                        //c = Color.FromArgb(c.R, c.G, 200);
-                        //g.FillRectangle(new SolidBrush(c2), rect2);
-                        //if (count > 50)
-                        //    break;
-                        //count++;
                     }
-
-                    // draw cross
-
-                    //for (int i = 0; i <= maxXCells; i++)
-                    //{
-                    //    g.DrawLine(pen, (i * boxSize), 0, i * boxSize, boxSize * maxYCells);
-                    //}
-
-                    //for (int i = 0; i <= maxYCells; i++)
-                    //{
-                    //    g.DrawLine(pen, 0, (i * boxSize), boxSize * maxXCells, i * boxSize);
-                    //}
-
-
+                    
                 }
 
                 bmp.Save("test.png", ImageFormat.Png);
