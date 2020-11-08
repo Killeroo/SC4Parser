52db2f4f33e07188d2f8dd15d6f63db305ffec0e
Matthew Carney <matthewcarney64@gmail.com>
2020-11-08 17:02:48 +0000

    Updated AssemblyInfo.cs

--- SC4Parser/Properties/AssemblyInfo.cs
+++ SC4Parser/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SC4Parser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SimCity 4 savegame parsing library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Matthew Carney [matthewcarney64@gmail.com]")]
 [assembly: AssemblyProduct("SC4Parser")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Matthew Carney 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
