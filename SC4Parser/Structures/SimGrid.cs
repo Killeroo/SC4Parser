d73eb7350756d0442e7de2bdff143e7f3bed9a3f
Matthew Carney <matthewcarney64@gmail.com>
2023-01-23 00:27:26 +0000

    Added initial SimGrid implementation

---
+++ SC4Parser/Structures/SimGrid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SC4Parser.Structures
+{
+    //https://wiki.sc4devotion.com/index.php?title=SimGrid
+    public class SimGrid
+    {
+        public uint Size { get; private set; }
+        public uint CRC;
+        public uint MemoryAddress;
+        public uint MajorVersion;
+        public uint TypeId;
+        public uint DataId;
+        public uint Resolution;
+        public uint ResolutionPower;
+        public uint SizeX;
+        public uint SizeY;
+        public object[][] Values;
+    }
+}
